a58059ae36e06661131103650599099e4c0f3f4a
Maël Chiotti <44336112+maelchiotti@users.noreply.github.com>
2024-02-09 19:30:04 +0100

    Fix navigation bar transparency

--- lib/pages/settings/settings_page.dart
+++ lib/pages/settings/settings_page.dart
@@ -23,123 +23,121 @@ class _SettingsPageState extends ConsumerState<SettingsPage> {
 
   @override
   Widget build(BuildContext context) {
-    return Padding(
-      padding: Paddings.custom.bottomSystemUi,
-      child: SettingsList(
-        platform: DevicePlatform.android,
-        lightTheme: SettingsThemeData(
-          settingsListBackground: Theme.of(context).colorScheme.background,
-        ),
-        darkTheme: SettingsThemeData(
-          settingsListBackground: Theme.of(context).colorScheme.background,
-        ),
-        sections: [
-          SettingsSection(
-            title: Text(localizations.settings_appearance),
-            tiles: [
-              SettingsTile.navigation(
-                leading: const Icon(Icons.palette),
-                title: Text(localizations.settings_theme),
-                value: Text(ThemeManager().themeModeName),
-                onPressed: (context) async {
-                  await interactions.selectTheme(context);
-                  setState(() {});
-                },
-              ),
-              if (ThemeManager().isDynamicThemingAvailable)
-                SettingsTile.switchTile(
-                  leading: const Icon(Icons.bolt),
-                  title: Text(localizations.settings_dynamic_theming),
-                  description: Text(localizations.settings_dynamic_theming_description),
-                  initialValue: ThemeManager().useDynamicTheming,
-                  onToggle: interactions.toggleDynamicTheming,
-                ),
-              SettingsTile.switchTile(
-                leading: const Icon(Icons.nightlight),
-                title: Text(localizations.settings_black_theming),
-                description: Text(localizations.settings_black_theming_description),
-                initialValue: ThemeManager().useBlackTheming,
-                onToggle: (toggled) {
-                  interactions.toggleBlackTheming(toggled);
-                  setState(() {});
-                },
-              ),
-              SettingsTile.navigation(
-                leading: const Icon(Icons.language),
-                title: Text(localizations.settings_language),
-                value: Text(Localizations.localeOf(context).nativeDisplayLanguage.capitalized),
-                onPressed: interactions.selectLanguage,
-              ),
-            ],
-          ),
-          SettingsSection(
-            title: Text(localizations.settings_behavior),
-            tiles: [
-              SettingsTile.navigation(
-                leading: const Icon(Icons.warning),
-                title: Text(localizations.settings_confirmations),
-                value: Text(Confirmations.fromPreferences().title),
-                onPressed: (context) async {
-                  await interactions.selectConfirmations(context);
-                  setState(() {});
-                },
-              ),
-              SettingsTile.navigation(
-                leading: const Icon(Icons.keyboard),
-                title: Text(localizations.settings_shortcuts),
-                value: Text(localizations.settings_shortcuts_description),
-                onPressed: interactions.showShortcuts,
-              ),
-            ],
-          ),
-          SettingsSection(
-            title: Text(localizations.settings_backup),
-            tiles: [
-              SettingsTile.navigation(
-                leading: const Icon(Icons.file_download),
-                title: Text(localizations.settings_export),
-                value: Text(localizations.settings_export_description),
-                onPressed: interactions.backup,
-              ),
-              SettingsTile.navigation(
-                leading: const Icon(Icons.file_upload),
-                title: Text(localizations.settings_import),
-                value: Text(localizations.settings_import_description),
-                onPressed: interactions.restore,
-              ),
-            ],
-          ),
-          SettingsSection(
-            title: Text(localizations.settings_about),
-            tiles: [
-              SettingsTile(
-                leading: const Icon(Icons.info),
-                title: Text(localizations.app_name),
-                value: Text(InfoManager().appVersion),
-                onPressed: interactions.showAbout,
-              ),
-              SettingsTile(
-                leading: const Icon(SimpleIcons.github),
-                title: Text(localizations.settings_github),
-                value: Text(localizations.settings_github_description),
-                onPressed: interactions.openGitHub,
-              ),
-              SettingsTile(
-                leading: const Icon(Icons.balance),
-                title: Text(localizations.settings_licence),
-                value: Text(localizations.settings_licence_description),
-                onPressed: interactions.openLicense,
-              ),
-              SettingsTile(
-                leading: const Icon(Icons.bug_report),
-                title: Text(localizations.settings_issue),
-                value: Text(localizations.settings_issue_description),
-                onPressed: interactions.openIssues,
-              ),
-            ],
-          ),
-        ],
+    return SettingsList(
+      platform: DevicePlatform.android,
+      contentPadding: Paddings.custom.bottomSystemUi,
+      lightTheme: SettingsThemeData(
+        settingsListBackground: Theme.of(context).colorScheme.background,
       ),
+      darkTheme: SettingsThemeData(
+        settingsListBackground: Theme.of(context).colorScheme.background,
+      ),
+      sections: [
+        SettingsSection(
+          title: Text(localizations.settings_appearance),
+          tiles: [
+            SettingsTile.navigation(
+              leading: const Icon(Icons.palette),
+              title: Text(localizations.settings_theme),
+              value: Text(ThemeManager().themeModeName),
+              onPressed: (context) async {
+                await interactions.selectTheme(context);
+                setState(() {});
+              },
+            ),
+            if (ThemeManager().isDynamicThemingAvailable)
+              SettingsTile.switchTile(
+                leading: const Icon(Icons.bolt),
+                title: Text(localizations.settings_dynamic_theming),
+                description: Text(localizations.settings_dynamic_theming_description),
+                initialValue: ThemeManager().useDynamicTheming,
+                onToggle: interactions.toggleDynamicTheming,
+              ),
+            SettingsTile.switchTile(
+              leading: const Icon(Icons.nightlight),
+              title: Text(localizations.settings_black_theming),
+              description: Text(localizations.settings_black_theming_description),
+              initialValue: ThemeManager().useBlackTheming,
+              onToggle: (toggled) {
+                interactions.toggleBlackTheming(toggled);
+                setState(() {});
+              },
+            ),
+            SettingsTile.navigation(
+              leading: const Icon(Icons.language),
+              title: Text(localizations.settings_language),
+              value: Text(Localizations.localeOf(context).nativeDisplayLanguage.capitalized),
+              onPressed: interactions.selectLanguage,
+            ),
+          ],
+        ),
+        SettingsSection(
+          title: Text(localizations.settings_behavior),
+          tiles: [
+            SettingsTile.navigation(
+              leading: const Icon(Icons.warning),
+              title: Text(localizations.settings_confirmations),
+              value: Text(Confirmations.fromPreferences().title),
+              onPressed: (context) async {
+                await interactions.selectConfirmations(context);
+                setState(() {});
+              },
+            ),
+            SettingsTile.navigation(
+              leading: const Icon(Icons.keyboard),
+              title: Text(localizations.settings_shortcuts),
+              value: Text(localizations.settings_shortcuts_description),
+              onPressed: interactions.showShortcuts,
+            ),
+          ],
+        ),
+        SettingsSection(
+          title: Text(localizations.settings_backup),
+          tiles: [
+            SettingsTile.navigation(
+              leading: const Icon(Icons.file_download),
+              title: Text(localizations.settings_export),
+              value: Text(localizations.settings_export_description),
+              onPressed: interactions.backup,
+            ),
+            SettingsTile.navigation(
+              leading: const Icon(Icons.file_upload),
+              title: Text(localizations.settings_import),
+              value: Text(localizations.settings_import_description),
+              onPressed: interactions.restore,
+            ),
+          ],
+        ),
+        SettingsSection(
+          title: Text(localizations.settings_about),
+          tiles: [
+            SettingsTile(
+              leading: const Icon(Icons.info),
+              title: Text(localizations.app_name),
+              value: Text(InfoManager().appVersion),
+              onPressed: interactions.showAbout,
+            ),
+            SettingsTile(
+              leading: const Icon(SimpleIcons.github),
+              title: Text(localizations.settings_github),
+              value: Text(localizations.settings_github_description),
+              onPressed: interactions.openGitHub,
+            ),
+            SettingsTile(
+              leading: const Icon(Icons.balance),
+              title: Text(localizations.settings_licence),
+              value: Text(localizations.settings_licence_description),
+              onPressed: interactions.openLicense,
+            ),
+            SettingsTile(
+              leading: const Icon(Icons.bug_report),
+              title: Text(localizations.settings_issue),
+              value: Text(localizations.settings_issue_description),
+              onPressed: interactions.openIssues,
+            ),
+          ],
+        ),
+      ],
     );
   }
 }
