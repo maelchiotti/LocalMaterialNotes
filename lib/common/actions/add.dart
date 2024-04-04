4685699c27e13380c6253e8f4bf8af70df3a48c0
Maël Chiotti <44336112+maelchiotti@users.noreply.github.com>
2024-04-05 00:19:50 +0200

    fix: remove prints

--- lib/common/actions/add.dart
+++ lib/common/actions/add.dart
@@ -9,16 +9,16 @@ import 'package:localmaterialnotes/providers/current_note/current_note_provider.
 
 Future<void> addNote(BuildContext context, WidgetRef ref, {String? content}) async {
   exitSelectionMode(ref);
-  print("1");
+
   final note = content == null ? Note.empty() : Note.content(content);
-  print("2");
+
   ref.read(currentNoteProvider.notifier).set(note);
-  print("3");
+
   if (!context.mounted) return;
-  print("4");
+
   final editorRoute = RouterRoute.editor.fullPath!;
   final editorParameters = EditorParameters.from({'readonly': false, 'autofocus': true});
-  print("5");
+
   // If the editor is already opened with another note, replace the route with the new editor
   RouterRoute.isEditor
       ? context.pushReplacement(editorRoute, extra: editorParameters)
