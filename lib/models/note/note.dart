372163b850d67c8d5e1db0b38027036ba8831b61
Maël Chiotti <44336112+maelchiotti@users.noreply.github.com>
2024-05-15 23:48:25 +0200

    style: fix analysis

--- lib/models/note/note.dart
+++ lib/models/note/note.dart
@@ -6,9 +6,8 @@ import 'package:fuzzywuzzy/fuzzywuzzy.dart';
 import 'package:isar/isar.dart';
 import 'package:json_annotation/json_annotation.dart';
 import 'package:localmaterialnotes/utils/constants/constants.dart';
+import 'package:localmaterialnotes/utils/preferences/preference_key.dart';
 import 'package:localmaterialnotes/utils/preferences/sort_method.dart';
-
-import '../../utils/preferences/preference_key.dart';
 
 part 'note.g.dart';
 
