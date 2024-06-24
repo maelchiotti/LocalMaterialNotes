bf41dc719a400c95c50b207874fcee22e8133654
Maël Chiotti <44336112+maelchiotti@users.noreply.github.com>
2024-06-24 16:13:01 +0200

    chore: improve generate_full_description.py script

--- fastlane/metadata/android/generate_full_description.py
+++ fastlane/metadata/android/generate_full_description.py
@@ -4,22 +4,30 @@ import minify_html
 import os
 import yaml
 
-root = os.path.join("fastlane", "metadata", "android")
-for item in os.listdir(root):
-    if os.path.isdir(os.path.join(root, item)):
-        yaml_filepath = os.path.join(root, item, "full_description.yaml")
-        with open(yaml_filepath) as yaml_stream:
-            try:
-                # Read and minimize the description inside the YAML file
-                yaml_file = yaml.safe_load(yaml_stream)
-                full_description = yaml_file["full_description"]
-                full_description_minified = minify_html.minify(
-                    full_description, keep_closing_tags=True
-                )
+root = os.getcwd()
+for language in os.listdir(root):
+    if not os.path.isdir(os.path.join(root, language)):
+        continue
 
-                # Write the description into the text file
-                text_filepath = os.path.join(root, item, "full_description.txt")
-                with open(text_filepath, mode="w") as text_file:
-                    text_file.write(full_description_minified)
-            except yaml.YAMLError as exception:
-                print(exception)
+    yaml_filepath = os.path.join(root, language, "full_description.yaml")
+
+    if not os.path.isfile(yaml_filepath):
+        continue
+
+    with open(yaml_filepath) as yaml_stream:
+        try:
+            # Read and minimize the description inside the YAML file
+            yaml_file = yaml.safe_load(yaml_stream)
+            full_description = yaml_file["full_description"]
+            full_description_minified = minify_html.minify(
+                full_description, keep_closing_tags=True
+            )
+
+            # Write the description into the text file
+            text_filepath = os.path.join(root, language, "full_description.txt")
+            with open(text_filepath, mode="w") as text_file:
+                text_file.write(full_description_minified)
+
+            print("Generated full_description.txt for language " + language)
+        except yaml.YAMLError as exception:
+            print(exception)
