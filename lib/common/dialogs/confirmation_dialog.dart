7557e0abef3029b2700c717ba222ce895e4b32dd
Maël Chiotti <44336112+maelchiotti@users.noreply.github.com>
2024-08-09 17:12:11 +0200

    docs: add dart doc for dialogs

--- lib/common/dialogs/confirmation_dialog.dart
+++ lib/common/dialogs/confirmation_dialog.dart
@@ -2,6 +2,11 @@ import 'package:flutter/material.dart';
 import 'package:localmaterialnotes/utils/constants/constants.dart';
 import 'package:localmaterialnotes/utils/preferences/enums/confirmations.dart';
 
+/// Shows the confirmation dialog to ask the user for a confirmation on an action.
+///
+/// Returns `true` if the user confirms the action, `false` otherwise.
+///
+/// The [title], [body] and [confirmText] depend on the action for which the confirmation must be obtained.
 Future<bool> _showConfirmationDialog(
   String title,
   String body,
@@ -35,6 +40,16 @@ Future<bool> _showConfirmationDialog(
       false;
 }
 
+/// Asks the user for a confirmation on an action.
+///
+/// Returns `true` if the confirmation is not needed according to the user settings, without showing
+/// the confirmation dialog. If the confirmation is needed, returns `true` if the user confirms the action,
+/// `false` otherwise.
+///
+/// The [title], [body] and [confirmText] depend on the action for which the confirmation must be obtained.
+///
+/// An action is [irreversible] if its consequences cannot be reversed, such as permanently deleting a note
+/// or emptying the bin.
 Future<bool> askForConfirmation(
   String title,
   String body,
