80a5efc89af22805b66d3b0d2570bed37bf2ed65
Maël Chiotti <44336112+maelchiotti@users.noreply.github.com>
2024-08-09 17:13:10 +0200

    docs: add dart doc for FABs

--- lib/common/fabs/fab_add_note.dart
+++ lib/common/fabs/fab_add_note.dart
@@ -3,6 +3,7 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:localmaterialnotes/common/actions/add.dart';
 import 'package:localmaterialnotes/utils/constants/constants.dart';
 
+/// Floating action button to add a note.
 class FabAddNote extends ConsumerStatefulWidget {
   const FabAddNote({super.key});
 
