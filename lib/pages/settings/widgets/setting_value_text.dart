26d3dd44086dd934d6009fceba2925d97e89a5db
Maël Chiotti <44336112+maelchiotti@users.noreply.github.com>
2024-10-11 15:16:59 +0200

    [202] fix: setting value text color when disabled (#204)

--- lib/pages/settings/widgets/setting_value_text.dart
+++ lib/pages/settings/widgets/setting_value_text.dart
@@ -2,16 +2,26 @@ import 'package:flutter/material.dart';
 import 'package:localmaterialnotes/common/constants/paddings.dart';
 import 'package:localmaterialnotes/common/constants/sizes.dart';
 
-/// Body of a navigation setting tile showing the [value] of the setting with its optional [icon]
-/// and an optional [description].
+/// Body of a navigation setting tile.
+///
+/// Contains:
+///   - The [value] of the setting.
+///   - An optional [icon].
+///   - An optional [description].
+///
+/// If the setting tile is not [enabled], the text is shown with a subdued color.
 class SettingNavigationTileBody extends StatelessWidget {
   /// Default constructor.
   const SettingNavigationTileBody({
     super.key,
+    this.enabled = true,
     required this.value,
     this.description,
     this.icon,
   });
+
+  /// Whether the setting tile is disabled.
+  final bool enabled;
 
   /// Value of the setting.
   final String value;
@@ -24,6 +34,9 @@ class SettingNavigationTileBody extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final titleSmallTextTheme = Theme.of(context).textTheme.titleSmall;
+    final tertiaryColor = Theme.of(context).colorScheme.tertiary;
+
     return Column(
       crossAxisAlignment: CrossAxisAlignment.start,
       children: [
@@ -40,7 +53,9 @@ class SettingNavigationTileBody extends StatelessWidget {
             Expanded(
               child: Text(
                 value,
-                style: Theme.of(context).textTheme.titleSmall?.copyWith(color: Theme.of(context).colorScheme.tertiary),
+                style: titleSmallTextTheme?.copyWith(
+                  color: enabled ? tertiaryColor : titleSmallTextTheme.color?.withAlpha(100),
+                ),
               ),
             ),
           ],
