69bd16eae0f99c8d2d5b6eb6f175774fec1599a1
Maël Chiotti <44336112+maelchiotti@users.noreply.github.com>
2024-12-20 15:14:46 +0100

    [301] ci: build mimir

---
+++ scripts/mimir/fdroid_build_mimir.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env sh
+
+test -d .mimir || exit
+(
+  cd .mimir || exit
+
+  echo "Building mimir"
+
+  bash scripts/build-android.sh
+
+  mv ../EmbeddedMilliAndroid.tar.gz "$PUB_CACHE"/hosted/pub.dev/flutter_mimir-*/android/src/main/jniLibs
+)
