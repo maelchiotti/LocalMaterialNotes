eb42dfa1f5ce984963a6373d0dc336be42519b73
Maël Chiotti <44336112+maelchiotti@users.noreply.github.com>
2024-12-31 14:27:40 +0100

    [325] chore: improve mimir build

--- scripts/mimir/fdroid_build_mimir.sh
+++ scripts/mimir/fdroid_build_mimir.sh
@@ -3,14 +3,20 @@
 # Pass no parameters to build mimir for all architectures,
 # or pass the desired architectures (x86, x64, armv7, arm64)
 
+mkdirJniLibs() {
+  for dir in "$PUB_CACHE"/hosted/pub.dev/flutter_mimir-*/; do
+    echo "Creating directory $dir/android/src/main/jniLibs/$1"
+    mkdir -p "${dir}android/src/main/jniLibs/$1"
+  done
+}
+
 x86() {
   echo "Building mimir for x86"
 
   bash scripts/build-android.sh x86
 
-  cd platform-build
-  mv libembedded_milli_android_x86.so libembedded_milli.so
-  mv libembedded_milli.so "$PUB_CACHE"/hosted/pub.dev/flutter_mimir-*/android/src/main/jniLibs/x86/
+  mkdirJniLibs x86
+  mv platform-build/jniLibs/x86/libembedded_milli.so "$PUB_CACHE"/hosted/pub.dev/flutter_mimir-*/android/src/main/jniLibs/x86/
 }
 
 x64() {
@@ -18,9 +24,8 @@ x64() {
 
   bash scripts/build-android.sh x64
 
-  cd platform-build
-  mv libembedded_milli_android_x64.so libembedded_milli.so
-  mv libembedded_milli.so "$PUB_CACHE"/hosted/pub.dev/flutter_mimir-*/android/src/main/jniLibs/x86_64/
+  mkdirJniLibs x86_64
+  mv platform-build/jniLibs/x86_64/libembedded_milli.so "$PUB_CACHE"/hosted/pub.dev/flutter_mimir-*/android/src/main/jniLibs/x86_64/
 }
 
 armv7() {
@@ -28,9 +33,8 @@ armv7() {
 
   bash scripts/build-android.sh armv7
 
-  cd platform-build
-  mv libembedded_milli_android_armv7.so libembedded_milli.so
-  mv libembedded_milli.so "$PUB_CACHE"/hosted/pub.dev/flutter_mimir-*/android/src/main/jniLibs/armeabi-v7a/
+  mkdirJniLibs armeabi-v7a
+  mv platform-build/jniLibs/armeabi-v7a/libembedded_milli.so "$PUB_CACHE"/hosted/pub.dev/flutter_mimir-*/android/src/main/jniLibs/armeabi-v7a/
 }
 
 arm64() {
@@ -38,19 +42,14 @@ arm64() {
 
   bash scripts/build-android.sh arm64
 
-  cd platform-build
-  mv libembedded_milli_android_arm64.so libembedded_milli.so
-  mv libembedded_milli.so "$PUB_CACHE"/hosted/pub.dev/flutter_mimir-*/android/src/main/jniLibs/arm64-v8a/
+  mkdirJniLibs arm64-v8a
+  mv platform-build/jniLibs/arm64-v8a/libembedded_milli.so "$PUB_CACHE"/hosted/pub.dev/flutter_mimir-*/android/src/main/jniLibs/arm64-v8a/
 }
 
 test -d .mimir || exit
 cp .mimir-cargo.lock .mimir/Cargo.lock
 
-for dir in "$PUB_CACHE"/hosted/pub.dev/flutter_mimir-*; do
-  echo "Creating directory $dir/android/src/main/jniLibs"
-  mkdir "$dir"/android/src/main/jniLibs
-done
-
+bash scripts/build-android.sh
 (
   cd .mimir || exit
   if [ "$#" -eq 0 ]; then
@@ -80,16 +79,3 @@ done
     done
   fi
 )
-
-
-
-
-  echo "Building mimir"
-
-  bash scripts/build-android.sh
-
-  for dir in "$PUB_CACHE"/hosted/pub.dev/flutter_mimir-*; do
-    echo "Creating directory $dir/android/src/main/jniLibs"
-    mkdir "$dir"/android/src/main/jniLibs
-  done
-  mv platform-build/EmbeddedMilliAndroid.tar.gz "$PUB_CACHE"/hosted/pub.dev/flutter_mimir-*/android/src/main/jniLibs/
