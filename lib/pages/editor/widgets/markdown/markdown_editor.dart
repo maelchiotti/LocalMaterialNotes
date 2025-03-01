425cee89a96ea29435d402a5c7fc2a0e47b826ca
Maël Chiotti <44336112+maelchiotti@users.noreply.github.com>
2025-03-01 01:26:56 +0100

    [421] fix: update markdown widget (#422)

--- lib/pages/editor/widgets/markdown/markdown_editor.dart
+++ lib/pages/editor/widgets/markdown/markdown_editor.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
-import 'package:flutter_markdown/flutter_markdown.dart';
+import 'package:flutter_markdown_plus/flutter_markdown_plus.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:markdown/markdown.dart';
 
