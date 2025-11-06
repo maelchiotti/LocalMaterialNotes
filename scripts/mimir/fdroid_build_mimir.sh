484c7086224c809304f4fd852a72895d54635465
Maël Chiotti <44336112+maelchiotti@users.noreply.github.com>
2025-11-06 23:48:37 +0100

    chore: check native libraries build

--- scripts/mimir/fdroid_build_mimir.sh
+++ scripts/mimir/fdroid_build_mimir.sh
@@ -5,7 +5,7 @@
 
 mkdirJniLibs() {
   for dir in "$PUB_CACHE"/hosted/pub.dev/flutter_mimir-*/; do
-    echo "Creating directory $dir/android/src/main/jniLibs/$1"
+    echo "Creating directory ${dir}android/src/main/jniLibs/$1"
     mkdir -p "${dir}android/src/main/jniLibs/$1"
   done
 }
@@ -16,17 +16,16 @@ x86() {
   bash scripts/build-android.sh x86
 
   mkdirJniLibs x86
-  mv platform-build/jniLibs/x86/libembedded_milli.so "$PUB_CACHE"/hosted/pub.dev/flutter_mimir-*/android/src/main/jniLibs/x86/
+  mv .mimir/platform-build/jniLibs/x86/libembedded_milli.so "$PUB_CACHE"/hosted/pub.dev/flutter_mimir-*/android/src/main/jniLibs/x86/
 }
 
 x64() {
   echo "Building mimir for x64"
 
   bash scripts/build-android.sh x64
-  ls -l -R
 
   mkdirJniLibs x86_64
-  mv platform-build/jniLibs/x86_64/libembedded_milli.so "$PUB_CACHE"/hosted/pub.dev/flutter_mimir-*/android/src/main/jniLibs/x86_64/
+  mv .mimir/platform-build/jniLibs/x86_64/libembedded_milli.so "$PUB_CACHE"/hosted/pub.dev/flutter_mimir-*/android/src/main/jniLibs/x86_64/
 }
 
 armv7() {
@@ -35,7 +34,7 @@ armv7() {
   bash scripts/build-android.sh armv7
 
   mkdirJniLibs armeabi-v7a
-  mv platform-build/jniLibs/armeabi-v7a/libembedded_milli.so "$PUB_CACHE"/hosted/pub.dev/flutter_mimir-*/android/src/main/jniLibs/armeabi-v7a/
+  mv .mimir/platform-build/jniLibs/armeabi-v7a/libembedded_milli.so "$PUB_CACHE"/hosted/pub.dev/flutter_mimir-*/android/src/main/jniLibs/armeabi-v7a/
 }
 
 arm64() {
@@ -44,7 +43,7 @@ arm64() {
   bash scripts/build-android.sh arm64
 
   mkdirJniLibs arm64-v8a
-  mv platform-build/jniLibs/arm64-v8a/libembedded_milli.so "$PUB_CACHE"/hosted/pub.dev/flutter_mimir-*/android/src/main/jniLibs/arm64-v8a/
+  mv .mimir/platform-build/jniLibs/arm64-v8a/libembedded_milli.so "$PUB_CACHE"/hosted/pub.dev/flutter_mimir-*/android/src/main/jniLibs/arm64-v8a/
 }
 
 test -d .mimir || exit
