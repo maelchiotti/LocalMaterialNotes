1fba161bddb451b435fdc96d43f0ff880bec60b1
Maël Chiotti <44336112+maelchiotti@users.noreply.github.com>
2026-04-07 21:17:42 +0200

    [523] Reduce APK size (#529)
    * chore: removed unneeded font files
    
    * chore: enabled legacy compression
    
    * chore: prepare release
    
    * chore: upgrade flutter and dart versions
    
    * chore: upgrade dependencies versions
    
    * chore: upgrade to java 17
    
    * chore: upgrade mimir

--- lib/services/database_service.dart
+++ lib/services/database_service.dart
@@ -1,5 +1,6 @@
 import 'package:flutter_mimir/flutter_mimir.dart';
 import 'package:isar_community/isar.dart';
+import 'package:mimir/mimir.dart';
 import 'package:path_provider/path_provider.dart';
 
 import '../models/deprecated/note.dart';
